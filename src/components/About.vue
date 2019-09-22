8573b7eec62230c769f9855f8f2306a1783187b4
Gabriel Barbosa <gabriel.lemosbarbosa@gmail.com>
2019-09-22 13:07:36 -0300

    Scrum no about

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,11 +5,16 @@
         <div class="col-md-4">
           <h5 class="text-color-reverb">SOBRE A REVERB</h5>
           Organização que preza pelo desenvolvimento profissional dos nossos membros utilizando metodologias ágeis.
+          <div class="text-center"> 
+            <img src="../assets/_corpo.png" class="img-about">
+          </div>
         </div>
 
         <div class="col-md-4">
           <h5 class="text-color-reverb">AGILIDADE</h5>
           Os métodos ágeis tem sido usados em muitas organizações com o intuito de aumentar a produtividade, gerar resultados mais rapidamente, reduzir os riscos, etc. Na Reverb estamos adaptando a metodologia ágil SCRUM.
+          <h5 class="text-color-reverb">SCRUM</h5>
+          Scrum é uma metodologia ágil geralmente utilizada para gestão e planejamento de projetos de software. No Scrum, os projetos são dividos em ciclos chamados de Sprints. O Sprint representa um Time Box dentro do qual um conjunto de atividades deve ser executado.
         </div>
 
         <div class="col-md-4 scroll">
@@ -33,5 +38,9 @@ export default {
 .scroll {
     max-height: 350px;
     overflow-y: auto;
+}
+
+.img-about {
+  max-width: 70%;
 }
 </style>
