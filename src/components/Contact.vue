dfed2f27a0437226ff1f4e491e2ca6a70043e595
Gabriel Barbosa <gabriel.lemosbarbosa@gmail.com>
2019-09-22 08:32:45 -0300

    Footer menor

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="contact">
-    <div class="row pt-3 pb-3">
+    <div class="row pt-1 pb-1">
       <div class="col-md-2 text-center">
         <img src="../assets/_texto.png">
       </div>
       <div class="col-md-4 text-center">
-        <p class="text-color-reverb mt-0 mb-0">CONTATO</p>
-        <p class="text-light mt-0 mb-0">
+        <p class="text-color-reverb mt-0 mb-0">
           <i class="fa fa-envelope" aria-hidden="true"></i>
           contato.reverb.esports@gmail.com
         </p>
       </div>
       <div class="col-md-2 text-center">
-        <p class="text-color-reverb mt-0 mb-0">SOCIAL</p>
         <p class="text-light mt-0 mb-0">
           <a href="https://www.instagram.com/reverbesports" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
           <a href="https://www.twitter.com/reverbesports" target="_blank"><i class="fa fa-twitter pl-2" aria-hidden="true"></i></a>
         </p>
       </div>
       <div class="col-md-4 text-center">
-        <p class="text-color-reverb mt-0 mb-0"><i class="fa fa-map-marker pl-2" aria-hidden="true"></i></p>
-        <p class="text-color-reverb mt-0 mb-0">TERESÓPOLIS - RJ</p>
+        <p class="text-color-reverb mt-0 mb-0">
+          <i class="fa fa-map-marker pl-2" aria-hidden="true"></i>
+          TERESÓPOLIS - RJ
+        </p>
       </div>
     </div>
   </div>
@@ -34,6 +34,6 @@ export default {
 
 <style scoped>
 img {
-  max-width: 50%;
+  max-width: 35%;
 }
 </style>
