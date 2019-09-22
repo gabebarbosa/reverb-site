c25be62ba9d734eae3f21c2b40b8fbbdbb9c199d
Gabriel Barbosa <gabriel.lemosbarbosa@gmail.com>
2019-09-22 09:49:11 -0300

    sobre agilidade

--- src/components/About.vue
+++ src/components/About.vue
@@ -14,7 +14,7 @@
 
         <div class="col-md-4">
           <h5 class="text-color-reverb">AGILIDADE</h5>
-          -
+          Os métodos ágeis tem sido usados em muitas organizações com o intuito de aumentar a produtividade, gerar resultados mais rapidamente, reduzir os riscos, etc. Na Reverb estamos adaptando a metodologia ágil SCRUM.
         </div>
         
       </div>
