e553d3a039b6563c3bf94db72f4c0f6d90bd6cbe
Gabriel Barbosa <gabriel.lemosbarbosa@gmail.com>
2019-09-21 22:27:20 -0300

    melhorando o about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
     <div class="card m-5">
-      <div class="card-body">
-        <h5 class="text-color-reverb">SOBRE A REVERB</h5>
-        Organização que preza pelo desenvolvimento profissional dos nossos membros utilizando metodologias ágeis.
+      <div class="card-body row">
+        <div class="col-md-4">
+          <h5 class="text-color-reverb">SOBRE A REVERB</h5>
+          Organização que preza pelo desenvolvimento profissional dos nossos membros utilizando metodologias ágeis.
+        </div>
+
+        <div class="col-md-4">
+          <h5 class="text-color-reverb">VALORES</h5>
+          -
+        </div>
+
+        <div class="col-md-4">
+          <h5 class="text-color-reverb">AGILIDADE</h5>
+          -
+        </div>
+        
       </div>
     </div>
   </div>
