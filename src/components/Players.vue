3b90013ac329736058e45b4119a073c21db3a1df
Gabriel Barbosa <gabriel.lemosbarbosa@gmail.com>
2019-09-22 09:35:47 -0300

    ajuste na imagem no responsivo

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -7,15 +7,15 @@
           <div class="col-md-4" v-for="t in team" :key="t.nome">
             <div class="card m-1">
               <div class="card-body row">
-                <div class="col-md-4">
+                <div class="col-md-4 text-center">
                   <img v-if="t.img === null || t.img === undefined" src="../assets/_wave_3.png" class="rounded-circle img-perfil">
                   <img v-if="t.img !== null && t.img !== undefined" :src="t.img" class="rounded-circle img-perfil">
                 </div>
                 <div class="col-md-8">
                   <h5 class="text-color-reverb mb-0 pb-0">
                     {{t.nick}}
-                    <a v-if="t.instagram !== null" :href="'https://www.instagram.com/' + t.instagram" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                    <a v-if="t.twitter !== null" :href="'https://www.twitter.com/' + t.twitter" target="_blank"><i class="fa fa-twitter pl-2" aria-hidden="true"></i></a>
+                    <a v-if="t.instagram !== null && t.instagram !== undefined" :href="'https://www.instagram.com/' + t.instagram" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a v-if="t.twitter !== null && t.twitter !== undefined" :href="'https://www.twitter.com/' + t.twitter" target="_blank"><i class="fa fa-twitter pl-2" aria-hidden="true"></i></a>
                   </h5>
                   <p class="mt-0 pt-0 mb-0 pb-0">{{t.nome}}</p>
                   <p class="mt-0 pt-0 mb-0 pb-0">{{t.funcao}}</p>
